66b20736964bd456be3dcec37b6818f1374f12ce
unex <7575866+unex@users.noreply.github.com>
2026-04-28 21:12:11 -0600

    update "scripts" for archlinux, other quality of life improvements

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -5,18 +5,25 @@
 # Author        : Harvey Lelliott (@flip-dots)
 # Date          : 23/03/26
 # Usage         : ./patch.sh [ADB device (e.g 192.168.1.1:1234)]
-# 
+#
 # License       : MIT
 # Revision      : 1.0.0
 #
 set -euxo pipefail
+
+# Use portable in-place sed for macOS (BSD sed) and Linux (GNU sed).
+if sed --version >/dev/null 2>&1; then
+  SED_INPLACE=(sed -i)
+else
+  SED_INPLACE=(sed -i '')
+fi
 
 
 #############
 # Constants #
 #############
 
-FRIDA_VERSION="17.8.2"
+FRIDA_VERSION="17.8.3"
 
 UBER_APK_SIGNER_VERSION="1.3.0"
 
@@ -85,12 +92,20 @@ mkdir -p $FRIDA_FOLDER
 #######################
 echo "Downloading dependencies/tools"
 
-cd $FRIDA_FOLDER && wget "https://github.com/zer0def/undetected-frida/releases/download/${FRIDA_VERSION}/undetected-frida-gadget-${FRIDA_VERSION}-android-arm.so.xz"
-cd $FRIDA_FOLDER && wget "https://github.com/zer0def/undetected-frida/releases/download/${FRIDA_VERSION}/undetected-frida-gadget-${FRIDA_VERSION}-android-arm64.so.xz"
-cd $FRIDA_FOLDER && unxz "undetected-frida-gadget-${FRIDA_VERSION}-android-arm.so.xz"
-cd $FRIDA_FOLDER && unxz "undetected-frida-gadget-${FRIDA_VERSION}-android-arm64.so.xz"
+cd "$FRIDA_FOLDER"
+if [ ! -f "undetected-frida-gadget-${FRIDA_VERSION}-android-arm.so" ]; then
+  wget -O "undetected-frida-gadget-${FRIDA_VERSION}-android-arm.so.xz" "https://github.com/zer0def/undetected-frida/releases/download/${FRIDA_VERSION}/undetected-frida-gadget-${FRIDA_VERSION}-android-arm.so.xz"
+  unxz -f "undetected-frida-gadget-${FRIDA_VERSION}-android-arm.so.xz"
+fi
 
-cd $TOOLS_FOLDER && wget "https://github.com/patrickfav/uber-apk-signer/releases/download/v${UBER_APK_SIGNER_VERSION}/uber-apk-signer-${UBER_APK_SIGNER_VERSION}.jar"
+if [ ! -f "undetected-frida-gadget-${FRIDA_VERSION}-android-arm64.so" ]; then
+  wget -O "undetected-frida-gadget-${FRIDA_VERSION}-android-arm64.so.xz" "https://github.com/zer0def/undetected-frida/releases/download/${FRIDA_VERSION}/undetected-frida-gadget-${FRIDA_VERSION}-android-arm64.so.xz"
+  unxz -f "undetected-frida-gadget-${FRIDA_VERSION}-android-arm64.so.xz"
+fi
+
+if [ ! -f "${TOOLS_FOLDER}/uber-apk-signer-${UBER_APK_SIGNER_VERSION}.jar" ]; then
+  cd "$TOOLS_FOLDER" && wget -O "uber-apk-signer-${UBER_APK_SIGNER_VERSION}.jar" "https://github.com/patrickfav/uber-apk-signer/releases/download/v${UBER_APK_SIGNER_VERSION}/uber-apk-signer-${UBER_APK_SIGNER_VERSION}.jar"
+fi
 
 
 ########################
@@ -99,6 +114,7 @@ cd $TOOLS_FOLDER && wget "https://github.com/patrickfav/uber-apk-signer/releases
 
 # Pull Anker APKs from Phone
 echo "Extracting original APKs from phone..."
+rm -f "${APK_SOURCE_FOLDER}"/*.apk
 adb -s $DEVICE shell pm path com.anker.charging | sed 's/^package://' | tr -d '\r' | xargs -I {} adb -s $DEVICE pull {} $APK_SOURCE_FOLDER
 
 # Decompile main APK
@@ -124,14 +140,14 @@ cp "${WORKING_FOLDER}/frida_config.json" "${APK_DECOMPILED_FOLDER}/lib/armeabi-v
 cp "${WORKING_FOLDER}/frida_config.json" "${APK_DECOMPILED_FOLDER}/lib/arm64-v8a/libnative-utils.config.so"
 
 # Add Frida gadget to app startup
-sed -i '' '34c \
+"${SED_INPLACE[@]}" '34c \
 const-string v0, "native-utils"\
 invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\
 ' "${APK_DECOMPILED_FOLDER}/smali/s/h/e/l/l/A.smali"
 
 # Modify manifest to enable Frida loading
-sed -i '' '/<application/,/>/ s/android:allowBackup="false"/android:allowBackup="true"/' "${APK_DECOMPILED_FOLDER}/AndroidManifest.xml"
-sed -i '' '/<application/,/>/ s/android:extractNativeLibs="false"/android:extractNativeLibs="true" android:debuggable="true"/' "${APK_DECOMPILED_FOLDER}/AndroidManifest.xml"
+"${SED_INPLACE[@]}" '/<application/,/>/ s/android:allowBackup="false"/android:allowBackup="true"/' "${APK_DECOMPILED_FOLDER}/AndroidManifest.xml"
+"${SED_INPLACE[@]}" '/<application/,/>/ s/android:extractNativeLibs="false"/android:extractNativeLibs="true" android:debuggable="true"/' "${APK_DECOMPILED_FOLDER}/AndroidManifest.xml"
 
 
 ##########################
@@ -143,20 +159,20 @@ echo "Re-packaging and re-signing APK..."
 apktool b -o "${APK_PATCHED_FOLDER}/base.apk" ${APK_DECOMPILED_FOLDER}
 
 # Re-sign APKs
-java -jar "${TOOLS_FOLDER}/uber-apk-signer-${UBER_APK_SIGNER_VERSION}.jar" -o $APK_SIGNED_FOLDER --allowResign --apks \
-   "${APK_PATCHED_FOLDER}/base.apk" \
-   "${APK_SOURCE_FOLDER}/split_config.arm64_v8a.apk" \
-   "${APK_SOURCE_FOLDER}/split_config.en.apk" \
-   "${APK_SOURCE_FOLDER}/split_config.xxhdpi.apk" \
-   "${APK_SOURCE_FOLDER}/split_flutter_assets_pack.apk"
+mapfile -t SOURCE_SPLITS < <(find "$APK_SOURCE_FOLDER" -maxdepth 1 -type f -name 'split*.apk' | sort)
+
+java -jar "${TOOLS_FOLDER}/uber-apk-signer-${UBER_APK_SIGNER_VERSION}.jar" -o "$APK_SIGNED_FOLDER" --allowResign --apks \
+  "${APK_PATCHED_FOLDER}/base.apk" \
+  "${SOURCE_SPLITS[@]}"
 
 # Uninstall existing Anker app
 adb -s $DEVICE uninstall com.anker.charging
 
 # Install patched APKs
-adb -s $DEVICE install-multiple \
-    "${APK_SIGNED_FOLDER}/base-aligned-debugSigned.apk" \
-    "${APK_SIGNED_FOLDER}/split_config.arm64_v8a-aligned-debugSigned.apk" \
-    "${APK_SIGNED_FOLDER}/split_config.en-aligned-debugSigned.apk" \
-    "${APK_SIGNED_FOLDER}/split_config.xxhdpi-aligned-debugSigned.apk" \
-    "${APK_SIGNED_FOLDER}/split_flutter_assets_pack-aligned-debugSigned.apk"
+SIGNED_APKS=("${APK_SIGNED_FOLDER}/base-aligned-debugSigned.apk")
+for split_apk in "${SOURCE_SPLITS[@]}"; do
+  split_name="$(basename "${split_apk}" .apk)"
+  SIGNED_APKS+=("${APK_SIGNED_FOLDER}/${split_name}-aligned-debugSigned.apk")
+done
+
+adb -s "$DEVICE" install-multiple "${SIGNED_APKS[@]}"
